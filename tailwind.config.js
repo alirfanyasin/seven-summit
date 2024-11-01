f3cdd5e81aa305666f5714f6c64787cad354ce2a
alirfanyasin <risayunita21@gmail.com>
2024-11-01 21:39:51 +0700

    configure tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/preline/dist/*.js",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./public/*.js"],
   theme: {
     container: {
       center: true,
